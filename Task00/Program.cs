050c19db7798cfc22fceac4ccab49a573a999098
9Ctrl <artnik1955@gmail.com>
2022-09-04 21:23:30 +0300

    Tusk00 задача 0

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -9,9 +9,12 @@
 // -3 -> 9
 // -7 -> 49
 //ВАРИАНТ1 int num = int.Parse(Console.ReadLine());
+Console.Clear();
 Console.WriteLine("Введите целое число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 int square = num * num;
-Console.WriteLine('Квадрат числа = ');
+Console.WriteLine($"Квадрат числа {num} = {square}");
+
+//Console.WriteLine("Квадрат числа" + num + "=" = square); - 2 вариант конкатенация
 //от -2 147 483 648  до 2 147 483 647
 
