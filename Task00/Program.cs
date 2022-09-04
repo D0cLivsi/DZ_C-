ee0c126778ef41b30a8ee1d9f56d57cb42360c72
9Ctrl <artnik1955@gmail.com>
2022-09-04 20:03:17 +0300

    Ошибка сборки

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 0ю Напишите программу, которая на вход
+// 1. принимает число  
+// 2. выдаёт его квадрат (число
+// умноженное на само себя).
+// 3. вывод результата
+
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+//ВАРИАНТ1 int num = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int square = num * num;
+Console.WriteLine('Квадрат числа = ');
+//от -2 147 483 648  до 2 147 483 647
+
